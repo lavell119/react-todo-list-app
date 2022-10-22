a1ac6b60cdab1ddca2deb946cf4c5a7aefe913cf
Lee <leewhiz119@gmail.com>
2022-10-21 20:44:27 -0400

    completed app

--- src/App.js
+++ src/App.js
@@ -10,14 +10,27 @@ class App extends Component {
     ]
   }
   deleteTodo = (id) => {
-    console.log(id)
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    })
+    this.setState({
+      todos: todos
+
+    })
+  }
+  AddTodo = (todo) => {
+    todo.id = Math.random()
+    let todos = [...this.state.todos, todo]
+    this.setState({
+      todos: todos
+    })
   }
   render() {
     return (
       <div className="App container">
         <h1 className="center blue-text">Todo's</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
-        <AddTodo />
+        <AddTodo addTodo={this.AddTodo}/>
       </div>
     )
   }
